305a03b09dbbfd8f7ad25b44020633814559402d
charkhaniakash <charkhaniakash@gmail.com>
2025-10-25 23:31:26 +0530

    updated Url

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         "https://real-time-opinion-polling-platform.onrender.com",
-        "https://real-time-opinion-polling-platform.vercel.app/"
+        "https://real-time-opinion-polling-platform.vercel.app"
     ],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
